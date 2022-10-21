c5cdd4e6902c3b8942a5181235b101d574031207
MavelSterling <mavelynsl@gmail.com>
2022-10-21 02:21:18 -0500

    Se agrega el archivo de manejo de var

---
+++ variables.js
@@ -0,0 +1,28 @@
+// las variables se declaran con var 
+
+var nombre = "JS"
+console.log(nombre) // acceder a la variable nombre
+
+// declarar una variable
+var edad
+
+// inicializar una variable 
+edad = 25
+
+console.log(edad) // acceder la variable
+
+var elementos = ["Computador", "Celular"]
+
+console.log(elementos) // acceder al array
+console.log(elementos[1]) // acceder al array
+
+
+// se genera un objeto y se guarda en la variable persona
+var persona = {
+    nombre: "David",
+    edad: 25
+}
+
+// Se accede a la variable persona
+console.log(persona)
+
