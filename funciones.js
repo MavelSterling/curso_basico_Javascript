b4e298ce80ddcc6f6181d01a9ea6c85ed9d6b0f7
MavelSterling <mavelynsl@gmail.com>
2022-10-21 02:38:07 -0500

    Se agrega tipos de  funciones

---
+++ funciones.js
@@ -0,0 +1,49 @@
+// Funciones declarativas
+
+// asi se declara una funcion
+function miFuncionDeclarada(a){
+    return
+}
+
+function miFuncion(){
+    return 3
+}
+
+
+// -----------------------------
+function saludar(nombre){
+
+    console.log("Hola " + nombre) 
+}
+// Invocaciones
+saludar("JavaScript") //"Hola JavaScript"
+saludar("Platzi") // "Hola Platzi"
+
+
+//------------------------------
+function suma (a,b){
+    return a + b
+}
+
+// Invocar la  función suma
+var resultado1 = suma(2,3)
+var resultado2 = suma(4,6)
+var resultado3 = suma(10,12)
+
+console.log(resultado1) //5
+console.log(resultado2) //10
+console.log(resultado3) //22
+
+
+//Funciones de expresión
+// no se declaran variables adentro de una función
+
+
+var suma = function (a, b) {
+    return a + b
+  }
+  // Invocación
+  var resultado = suma(2, 2)
+  
+  console.log(resultado)
+
