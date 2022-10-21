d18ab73e56b5bd9bc9ed852ea100560eac8f3533
MavelSterling <mavelynsl@gmail.com>
2022-10-21 01:28:01 -0500

    se agrega archivo con los tipos de datos

--- Operaciones.js
+++ Operaciones.js
@@ -1,7 +1,7 @@
 
 // Operaciones
-console.log(4 + "7")
-4 * "7";
-2 + true;
-false - 3;
+console.log(4 + "7") //47
+console.log(4 * "7") //28
+console.log(2 + true) //3
+console.log(false - 3) //-3
 
---
+++ valores.js
@@ -0,0 +1,39 @@
+// VALORES PRIMITIVOS
+
+//Números
+
+console.log(45)
+console.log(3.14)
+
+// Strings - cadenas de texto, se imprime con "Hola" o 'Hola'
+
+console.log("Javascript")
+console.log('Javascript')
+
+// Booleanos
+
+console.log(true)
+console.log(false)
+
+// valores vacios 
+
+console.log(null)
+console.log(undefined) // tratar de evitarlo
+
+// VALORES NO PRIMITIVOS
+
+// arrays [] 
+
+console.log([2,3,4,5])
+console.log([2.4,3.5,4.1,5.3])
+
+// {}- tipo objeto
+console.log({nombre: 'Javascript'})
+
+// ver que tipo de dato es con typeof
+console.log(typeof 40) //number
+console.log(typeof "Javascript") // string
+console.log(typeof null)
+console.log(typeof true) //booleano
+console.log(typeof []) //objeto
+console.log(typeof {}) //objeto
