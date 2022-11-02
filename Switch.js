9e443232eaa338fe31000f685dc46e4e27fcecec
MavelSterling <mavelynsl@gmail.com>
2022-11-02 15:09:37 -0500

    Se otra solution

--- Switch.js
+++ Switch.js
@@ -142,8 +142,23 @@ export function solution(article) {
       return 'Artículo no encontrado'
     }
 }
-    
 
+export function solution(article) {
+
+  switch (article) {
+    case "computadora":
+      return "Con mi computadora puedo programar usando JavaScript"
+      break;
+    case "celular":
+      return "En mi celular puedo aprender usando la app de Platzi"
+      break;
+    case "cable":
+      return "¡Hay un cable en mi bota!"
+      break;
+    default:
+      return "Artículo no encontrado"
+   }
+}
 
       solution('computadora')
       solution('celular')
