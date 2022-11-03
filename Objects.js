707c6ba39d4f00572cc45089550fb893ac92dcdc
MavelSterling <mavelynsl@gmail.com>
2022-11-02 23:51:28 -0500

    Se agrega un ejemplo

--- Objects.js
+++ Objects.js
@@ -305,3 +305,83 @@ var otroAuto3 = new Auto("Ferrari", "Model N", 2018)
 console.log("Caracteristicas del auto 3:",otroAuto3)
 console.log("--------------------------------")
 console.log("--------------------------------")
+
+// ingresar 30 autos con sus atributos
+var marca = [
+    "Abarth",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",	
+   "Cadillac",
+    "Caterham",
+    "Chevrolet",
+    "Citroen",
+    "Dacia",
+    "Ferrari",
+    "Fiat",
+    "Ford",
+    "Honda",
+    "Infiniti",
+    "Isuzu",
+    "Iveco",
+    "Jaguar"   	 	 
+]
+
+var modelo = [
+    "C-Max",
+    "Fiesta",
+    "Focus",
+    "Mondeo",
+    "Ka",
+    "S-MA",
+   " B-MAX",
+    "Grand C-Max",
+    "Tourneo Custom",
+    "Kuga",
+    "Galaxy",
+    "Grand Tourneo Connect",
+    "Tourneo Connect",
+    "EcoSport",
+    "Tourneo Courier",
+    "Mustang",
+    "Transit Connect",
+    "Edge",
+    "Ka+"
+]
+
+var anio = [
+    "1988",
+    "1989",
+    "1978",
+    "1989",
+    "1928",
+    "1989",
+    "1968",
+    "1989",
+    "1888",
+    "1989",
+    "1288",
+    "1989",
+    "1938",
+    "1989",
+    "1988",
+    "1999",
+    "1983",
+    "1989",
+    "1918"
+]
+
+
+
+
+function auto(marca, modelo, anio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio}
+
+for(var i = 0; marca.length > i && modelo.length > i && anio.length > i; i++){
+    var nuevoAuto = new auto(marca[i], modelo[i], anio[i]);
+    console.log(nuevoAuto)
+}
